9b32973b90e58203b74fa2e59c18ea16019972a9
Sukalyan <sukalyandash1920@gmail.com>
2022-02-11 22:07:43 +0530

    UC-3 Inserting Values To Tables

---
+++ PayRollUC3.sql
@@ -0,0 +1,18 @@
+create database PayRoll_Service;
+
+--uc2 creating table
+create Table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+
+
+--uc -3 inserting values to table
+Insert into employee_payroll(name,salary,startDate) values
+('Rasmita',36000,'2022-12-12'),
+('Papuni',47000,'2022-08-18'),
+('Runu',45000,'2021-08-21'),
+('Rakshi',35000,'2021-08-19');
